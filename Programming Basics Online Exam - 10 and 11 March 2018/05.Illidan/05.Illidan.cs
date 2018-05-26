aba2db6da18cddf46ff3a5faccb8d6726d955d56
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:12:28 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/05.Illidan/05.Illidan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.Illidan
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int health = int.Parse(Console.ReadLine());
+            int totalPower = count * power;
+
+            if (totalPower < health)
+            {
+                Console.WriteLine($"You are not prepared! You need {health - totalPower} more points.");
+            }
+            else
+            {
+                Console.WriteLine($"Illidan has been slain! You defeated him with {totalPower - health} points.");
+            }
+        }
+    }
+}
