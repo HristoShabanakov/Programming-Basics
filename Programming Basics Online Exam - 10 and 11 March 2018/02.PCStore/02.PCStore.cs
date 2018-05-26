6af863c7dc4762549e56a82a6f53307d987e7a2f
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:08:34 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/02.PCStore/02.PCStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.PCStore
+{
+    class Program
+    {
+        static void Main()
+        {
+            double cpuPrice = double.Parse(Console.ReadLine());
+            double videoCardPrice = double.Parse(Console.ReadLine());
+            double ramMemory = double.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double cpuPriceBG = cpuPrice * 1.57;
+            double videoCardBG = videoCardPrice * 1.57;
+            double ramMemoryBG = ramMemory * 1.57;
+            double totalRamMemory = ramMemoryBG * ram;
+            double cpuPriceWithDiscount = cpuPriceBG - (cpuPriceBG * discount);
+            double videoCardPriceWithDiscount = videoCardBG - (videoCardBG * discount);
+            double total = cpuPriceWithDiscount + videoCardPriceWithDiscount + totalRamMemory;
+
+            Console.WriteLine($"Money needed - {total:f2} leva.");
+        }
+    }
+}
