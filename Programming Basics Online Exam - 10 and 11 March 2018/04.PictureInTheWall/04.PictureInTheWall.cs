7ab4d94a1309134141ed3cecaba505ef0d551831
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:11:15 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/04.PictureInTheWall/04.PictureInTheWall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04.PictureInTheWall
+{
+    class Program
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int side = int.Parse(Console.ReadLine());
+            int picturesCount = int.Parse(Console.ReadLine());
+
+            int holeArea = width * length;
+            int picturesArea = (side * side) * picturesCount;
+
+            if (holeArea < picturesArea)
+            {
+                Console.WriteLine($"The pictures don't fit in the hole. Picture area is {picturesArea - holeArea} bigger than hole area.");
+            }
+            else
+            {
+                Console.WriteLine($"The pictures fit in the hole. Hole area is {holeArea - picturesArea} bigger than pictures area.");
+            }
+        }
+    }
+}
