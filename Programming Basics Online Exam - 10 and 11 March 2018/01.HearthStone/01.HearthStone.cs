273f9f3218d67d66046e55b4a36ebf14663aae0a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:06:59 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/01.HearthStone/01.HearthStone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.HearthStone
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pauseTime = int.Parse(Console.ReadLine());
+            double packetPrice = double.Parse(Console.ReadLine());
+            double questPrice = double.Parse(Console.ReadLine());
+            double coffeePrice = double.Parse(Console.ReadLine());
+
+            int restingTime = 10;
+            int breakTime = pauseTime - 5;
+            int totalTime = breakTime - restingTime;
+            double totalPackets = 3 * packetPrice;
+            double totalQuests = 2 * questPrice;
+            double total = totalPackets + totalQuests + coffeePrice;
+
+            Console.WriteLine($"{total:F2}");
+            Console.WriteLine($"{totalTime}");
+        }
+    }
+}
