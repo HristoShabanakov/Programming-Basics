5720e6b0024b12d06301383377ce4f707e63ff32
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:13:22 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/07.SlotMachine/07.SlotMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07.SlotMachine
+{
+    class Program
+    {
+        static void Main()
+        {
+            char a = char.Parse(Console.ReadLine());
+            int numberOne = int.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+
+            int firstLetter = a + numberOne;
+            int secondLetter = b + numberTwo;
+            int thirdLetter = c + numberThree;
+
+            Console.WriteLine($"{(char)firstLetter}{(char)secondLetter}{(char)thirdLetter}");
+
+            if (firstLetter == '@' && secondLetter == '@' && thirdLetter == '@')
+            {
+                Console.WriteLine("!!! YOU LOSE EVERYTHING !!!");
+            }
+            else if (firstLetter == '7' && secondLetter == '7' && thirdLetter == '7')
+            {
+                Console.WriteLine("*** JACKPOT ***");
+            }
+        }
+    }
+}
