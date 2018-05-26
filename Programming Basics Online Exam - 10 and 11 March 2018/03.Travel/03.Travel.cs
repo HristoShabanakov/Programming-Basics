d18c038863707b5b3ca5f0ff07379a73eba0a9e3
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-26 18:10:04 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/03.Travel/03.Travel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Travel
+{
+    class Program
+    {
+        static void Main()
+        {
+            double distance = double.Parse(Console.ReadLine());
+            double truckSpeed = double.Parse(Console.ReadLine());
+            double speedDifference = double.Parse(Console.ReadLine());
+            double carSpeed = (speedDifference * 3.6) + truckSpeed;
+            double truckTime = Math.Ceiling( distance / truckSpeed);
+            double carTime = Math.Ceiling(distance / carSpeed);
+            
+            Console.WriteLine($"The truck arrived after {truckTime} hours");
+            Console.WriteLine($"The car arrived after {carTime} hours");
+        }
+    }
+}
