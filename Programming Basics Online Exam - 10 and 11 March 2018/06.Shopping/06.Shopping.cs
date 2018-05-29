e5d3beb18d86a4e300287ab146e61afccc6b7587
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-05-29 22:07:48 +0200

    Programming Basics Online Exam - 10 and 11 March 2018

---
+++ Programming Basics Online Exam - 10 and 11 March 2018/06.Shopping/06.Shopping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.Shopping
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int chocoCount = int.Parse(Console.ReadLine());
+            double milk = double.Parse(Console.ReadLine());
+
+            double chocoPrice = chocoCount * 0.65;
+            double milkPrice = milk * 2.7;
+            double mandarineCount = Math.Floor(chocoCount - (chocoCount * 0.35));
+            double mandarinePrice = mandarineCount * 0.20;
+            double total = chocoPrice + milkPrice + mandarinePrice;
+
+            if(total <= budget)
+            {
+                Console.WriteLine($"You got this, {budget-total:f2} money left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {total-budget:f2} more!");
+            }
+        }
+    }
+}
