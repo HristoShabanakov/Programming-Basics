d043c3708eaca1df80d5b042eb834b3bf832cd1f
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-12 22:26:15 +0200

    Programming Basics Online Exam - 28 and 29 April 2018

---
+++ Programming Basics Online Exam - 28 and 29 April 2018/01.ProgrammingBook/01.ProgrammingBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double singlePage = double.Parse(Console.ReadLine());
+            double singleCover = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+            double design = double.Parse(Console.ReadLine());
+            double advanceMoney = double.Parse(Console.ReadLine());
+
+            double preciseDiscount = discount / 100;
+            double bookPrice = singlePage * 899 + singleCover * 2;
+            double bookCost = bookPrice * (1 - preciseDiscount) + design;
+            double preciseAdvance = advanceMoney / 100;
+            double totalPrice = bookCost * (1 - preciseAdvance);
+
+
+            Console.WriteLine($"Avtonom should pay {totalPrice:F2} BGN.");
+        }
+    }
+}
