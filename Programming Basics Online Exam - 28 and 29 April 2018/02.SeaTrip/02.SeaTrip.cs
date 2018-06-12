0220e3f2b4fecf06a62526443217cd00dbc901ec
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-12 22:28:01 +0200

    Programming Basics Online Exam - 28 and 29 April 2018

---
+++ Programming Basics Online Exam - 28 and 29 April 2018/02.SeaTrip/02.SeaTrip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.SeaTrip
+{
+    class Program
+    {
+        static void Main()
+        {
+            double foodMoney = double.Parse(Console.ReadLine());
+            double souvenirMoney = double.Parse(Console.ReadLine());
+            double hotelMoney = double.Parse(Console.ReadLine());
+
+            double totalDistance = 420.0 / 100.0 * 7 * 1.85;
+            double foodAndSouvenirMoney = foodMoney * 3 + souvenirMoney * 3;
+            double accommodationMoney = hotelMoney * 0.9 + hotelMoney * 0.85 + hotelMoney * 0.8;
+            double totalMoneySpent = totalDistance + foodAndSouvenirMoney + accommodationMoney;
+            
+
+            Console.WriteLine($"Money needed: {totalMoneySpent:f2}");
+        }
+    }
+}
