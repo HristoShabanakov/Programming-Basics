cba119db79e8b1642e74a199a43fbf60f7aa23a2
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-13 23:02:34 +0200

    Programming Basics Online Exam - 28 and 29 April 2018

---
+++ Programming Basics Online Exam - 28 and 29 April 2018/08.RunningInThePark/08.RunningInThePark.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.RunningInThePark
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double totalDistance = 0;
+            double totalMinutes = 0;
+            double totalCalories = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int minutes = int.Parse(Console.ReadLine());
+                double distance = double.Parse(Console.ReadLine());
+                string measure = Console.ReadLine();
+
+                if (measure == "m")
+                {
+                    totalDistance += distance / 1000;
+                    totalMinutes += minutes;
+                    totalCalories += minutes / 20.0 * 400;
+                }
+
+                else if (measure == "km")
+                {
+                    totalDistance += distance;
+                    totalMinutes += minutes;
+                    totalCalories += minutes / 20.0 * 400;
+                }
+            }
+            Console.WriteLine($"He ran {totalDistance:f2}km for {totalMinutes} minutes and burned {totalCalories} calories.");
+        }
+    }
+}
