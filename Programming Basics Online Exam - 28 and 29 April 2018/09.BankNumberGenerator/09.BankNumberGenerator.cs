6d745dd35351e3a135e9fbd0ffe3ec6f10ede499
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-15 22:26:14 +0200

    Programming Basics Online Exam - 28 and 29 April 2018

---
+++ Programming Basics Online Exam - 28 and 29 April 2018/09.BankNumberGenerator/09.BankNumberGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _09.BankNumberGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstRow = int.Parse(Console.ReadLine());
+            char secondRow = char.Parse(Console.ReadLine().ToUpper());
+            char thirdRow = char.Parse(Console.ReadLine().ToLower());
+            char forthRow = char.Parse(Console.ReadLine().ToUpper());
+            int fithRow = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = firstRow; i <= 99; i++)
+            {
+                for (char j = secondRow; j <='Z' ; j++)
+                {
+                    for (char k = thirdRow; k <= 'z'; k++)
+                    {
+                        for (char l = forthRow; l <= 'Z'; l++)
+                        {
+                            for (int m = fithRow; m >= 10; m--)
+                            {
+                                if( i % 10== 2 && m % 10 == 5)
+                                {
+                                    count+=1;
+                                }
+
+                                if(count == n)
+                                {
+                                    Console.WriteLine($"{i}{j}{k}{l}{m}");
+                                    return;
+                                }
+                                    
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
