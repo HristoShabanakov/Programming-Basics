bb06af0899a78437f8625c64d8c90a000a3dee36
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-20 06:51:10 +0200

    Programming Basics Online Exam - 16 and 17 June 2018

---
+++ Programming Basics Online Exam - 16 and 17 June 2018/17th June/01.CatDiet/01.CatDiet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.CatDiet
+{
+    class Program
+    {
+        static void Main()
+        {
+            double fat = double.Parse(Console.ReadLine());
+            double proteins = double.Parse(Console.ReadLine());
+            double carbohydrates = double.Parse(Console.ReadLine());
+            double calories = double.Parse(Console.ReadLine());
+            double water = double.Parse(Console.ReadLine());
+
+            double totalFat = fat * calories / 9 / 100.0;
+            double totalProteins = proteins * calories / 4 / 100.0;
+            double totalCarbohydrates = carbohydrates * calories / 4 / 100.0;
+            double totalFood = totalFat + totalProteins + totalCarbohydrates;
+            double caloriesPerGram = calories / totalFood;
+            double waterPercentage = water / 100.0;
+            double totalCalories = caloriesPerGram * (1 - waterPercentage);
+            
+            
+            Console.WriteLine($"{totalCalories:f4}");
+
+        }
+    }
+}
