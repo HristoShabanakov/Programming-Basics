cb6b0c2b2a079a5cb01391ada54e4b41df1c5b2b
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-21 22:00:33 +0200

    Programming Basics Exam - 25 June 2017

---
+++ Programming Basics Exam - 25 June 2017/04.Problem/04.Problem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.Problem
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double points = double.Parse(Console.ReadLine());
+            int total = 0;
+            int bonus = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                //For each even part bonus points * 2.
+                if (i % 2 == 0)
+                {
+                    bonus += input * 2;
+                }
+                else
+                {
+                    total += input;
+                }
+            }
+            double result = (bonus + total) * points;
+            Console.WriteLine($"The project prize was {result:f2} lv.");
+        }
+    }
+}
