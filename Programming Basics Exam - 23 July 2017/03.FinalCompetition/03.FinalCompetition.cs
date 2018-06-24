c092bf69504754d3b4964c5a5806d27830eb9581
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-24 20:10:22 +0200

    Programming Basics Exam - 23 July 2017

---
+++ Programming Basics Exam - 23 July 2017/03.FinalCompetition/03.FinalCompetition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _03.FinalCompetition
+{
+    class Program
+    {
+        static void Main()
+        {
+            double countDancers = double.Parse(Console.ReadLine());
+            double points = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string location = Console.ReadLine();
+
+            double money = 0;
+            double moneyAfterExpenses = 0;
+
+
+            switch (location)
+            {
+                case "Bulgaria":
+                    money += countDancers * points;
+                    if (season == "winter")
+                        moneyAfterExpenses = money - (money * 0.08);
+                    else if (season == "summer")
+                        moneyAfterExpenses = money - (money * 0.05);
+                    break;
+
+                case "Abroad":
+                    money += (countDancers * points);
+                    money = money + (money * 0.50);
+                    if (season == "summer")
+                        moneyAfterExpenses = money - (money * 0.10);
+                    else if (season == "winter")
+                        moneyAfterExpenses = money - (money * 0.15);
+                    break;
+            }
+            double moneyForCharity = moneyAfterExpenses * 0.75;
+            double leftMoney = moneyAfterExpenses - moneyForCharity;
+            double moneyForDancers = leftMoney / countDancers;
+
+            Console.WriteLine($"Charity - {moneyForCharity:f2}");
+            Console.WriteLine($"Money per dancer - {moneyForDancers:f2}");
+        }
+    }
+}
