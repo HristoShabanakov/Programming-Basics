38ebfbdf851b159b3c22789a89ef3bfa0babcb30
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-24 20:09:19 +0200

    Programming Basics Exam - 23 July 2017

---
+++ Programming Basics Exam - 23 July 2017/01.DanceHall/01.DanceHall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.DanceHall
+{
+    class Program
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double wardrobe = double.Parse(Console.ReadLine());
+
+            double hallSize = (length * 100) * (width * 100);
+            double wardrobeSize = (wardrobe * 100) * (wardrobe * 100);
+            double benchSize = hallSize / 10;
+            double freeSpace = hallSize - wardrobeSize - benchSize;
+            double dancersCount = freeSpace / (40 + 7000);
+
+            Console.WriteLine(Math.Floor(dancersCount));
+        }
+    }
+}
