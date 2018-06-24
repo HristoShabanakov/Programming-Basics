80e681ed67d62d832923c33bc215ddb69fa8f504
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-24 20:09:51 +0200

    Programming Basics Exam - 23 July 2017

---
+++ Programming Basics Exam - 23 July 2017/02.Choreography/02.Choreography.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.Choreography
+{
+    class Program
+    {
+        static void Main()
+        {
+            double steps = double.Parse(Console.ReadLine());
+            double dancers = double.Parse(Console.ReadLine());
+            double days = double.Parse(Console.ReadLine());
+
+            double stepsPerDay = Math.Ceiling((steps / days) / steps * 100);
+            double stepsPerDancer = stepsPerDay / dancers;
+
+            if (stepsPerDay <= 13)
+            {
+                Console.WriteLine($"Yes, they will succeed in that goal! {stepsPerDancer:f2}%.");
+            }
+            else
+            {
+                Console.WriteLine($"No, They will not succeed in that goal! Required {stepsPerDancer:f2}% steps to be learned per day.");
+            }
+        }
+    }
+}
