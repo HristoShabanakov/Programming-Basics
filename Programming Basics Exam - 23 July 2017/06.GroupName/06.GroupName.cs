65a502ccf86903bf0f6e8f479d0763f125b8c411
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-25 18:48:25 +0200

    Programming Basics Exam - 23 July 2017

---
+++ Programming Basics Exam - 23 July 2017/06.GroupName/06.GroupName.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06.GroupName
+{
+    class Program
+    {
+        static void Main()
+        {
+            char a = char.Parse(Console.ReadLine().ToUpper());
+            char b = char.Parse(Console.ReadLine().ToLower());
+            char c = char.Parse(Console.ReadLine().ToLower());
+            char d = char.Parse(Console.ReadLine().ToLower());
+            int e = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (char i = 'A'; i <= a; i++)
+            {
+                for (char j = 'a'; j <= b; j++)
+                {
+                    for (char k = 'a'; k <= c; k++)
+                    {
+                        for (char l = 'a'; l <= d; l++)
+                        {
+                            for (int m = 0; m <= e; m++)
+                            {
+                                if (i == a && j == b && k == c && l == d && m == e)
+                                {
+                                    break;
+                                }
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
