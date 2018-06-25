18e5b341c99dbc724aaf099801416ef6b1ff97d8
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-25 18:47:44 +0200

    Programming Basics Exam - 23 July 2017

---
+++ Programming Basics Exam - 23 July 2017/04.EnergyLoss/04.EnergyLoss.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _04.EnergyLoss
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int dancers = int.Parse(Console.ReadLine());
+
+            double wastedEnergy = 0;
+            double totalEnergy = 100 * n * dancers;
+
+            for (int trainingDays = 1; trainingDays <=n; trainingDays++)
+            {
+                int trainingHours = int.Parse(Console.ReadLine());
+
+                //Even training & even hour
+                if (trainingDays % 2 == 0 && trainingHours % 2 == 0)
+                {
+                    wastedEnergy += dancers * 68;
+                }
+                //Odd training & even hour
+                else if (trainingDays % 2 == 1 && trainingHours % 2 == 0)
+                {
+                    wastedEnergy += dancers * 49;
+                }
+                //Even training & odd hour
+                else if (trainingDays % 2 == 0 && trainingHours % 2 == 1)
+                {
+                    wastedEnergy += dancers * 65;
+                }
+                //Odd training & odd hour
+                else if (trainingDays % 2 == 1 && trainingHours % 2 == 1)
+                {
+                    wastedEnergy += dancers * 30;
+                }
+            }
+
+            double unusedEnergy = totalEnergy - wastedEnergy;
+            double unusedEnergyPerDancer = unusedEnergy / dancers / n;
+
+            if (unusedEnergyPerDancer > 50)
+            {
+                Console.WriteLine($"They feel good! Energy left: {unusedEnergyPerDancer:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"They are wasted! Energy left: {unusedEnergyPerDancer:f2}");
+            }
+        }
+    }
+}
