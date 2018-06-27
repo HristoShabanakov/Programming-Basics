c0e51753dee75539f085be5f5d70e44b254d7d83
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-27 21:58:07 +0200

    Programming Basics Exam - 03 September 2017

---
+++ Programming Basics Exam - 03 September 2017/04.Cake/04.Cake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04.Cake
+{
+    class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double pieces = width * lenght;
+
+            while (pieces >= 0)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    break;
+                }
+                //Everytime when someone take a piece of cake we reduce the total number if pieces.
+                pieces -= int.Parse(input);
+            }
+            if (pieces >= 0)
+            {
+                Console.WriteLine($"{pieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+            }
+
+        }
+    }
+}
