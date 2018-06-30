048ed667a1b0acfbee512144104f6037c35b0e43
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-30 11:58:52 +0200

    Programming Basics Exam - 03 September 2017

---
+++ Programming Basics Exam - 03 September 2017/06.Profit/06.Profit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.Profit
+{
+    class Program
+    {
+        static void Main()
+        {
+            double coinsOneLv = double.Parse(Console.ReadLine());
+            double coinsTwoLv = double.Parse(Console.ReadLine());
+            double coinsThreeLv = double.Parse(Console.ReadLine());
+            double sum = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= coinsOneLv; i++)
+            {
+                for (int j = 0; j <=coinsTwoLv; j++)
+                {
+                    for (int k = 0; k <=coinsThreeLv; k++)
+                    {
+                        if( i * 1 + j * 2 + k * 5 == sum)
+                        {
+                            Console.WriteLine($"{i} * 1 lv. + {j} * 2lv. + {k} * 5lv. = {sum} lv.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
