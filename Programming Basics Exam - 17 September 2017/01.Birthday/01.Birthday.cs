994942416fcf6210e1825a3f73d2930bbc797e50
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-06-30 12:27:06 +0200

    Programming Basics Exam - 17 September 2017

---
+++ Programming Basics Exam - 17 September 2017/01.Birthday/01.Birthday.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double aquariumVolume = (length * width * height) * 0.001;
+            double totalPercentage = percentage * 0.01;
+            double totalLittersWater = aquariumVolume * (1 - totalPercentage);
+
+            Console.WriteLine($"{totalLittersWater:f3}");
+        }
+    }
+}
