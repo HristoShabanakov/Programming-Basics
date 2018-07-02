c406eca05dc88db963912fd0c9ca3e295c599ef8
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-02 19:46:57 +0200

    Programming Basics Exam - 17 September 2017

---
+++ Programming Basics Exam - 17 September 2017/04.GrandpaStavri/04.GrandpaStavri.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.GrandpaStavri
+{
+    class Program
+    {
+        static void Main()
+        {
+            double days = double.Parse(Console.ReadLine());
+            double totalLitters = 0;
+            double littersPerDay = 0;
+
+
+            for (int i = 0; i < days; i++)
+            {
+                double quantity = double.Parse(Console.ReadLine());
+                double degrees = double.Parse(Console.ReadLine());
+                totalLitters += quantity;
+                littersPerDay += quantity * degrees;
+            }
+
+            double totalDegrees = littersPerDay / totalLitters;
+
+            Console.WriteLine($"Liter: {totalLitters:f2}");
+            Console.WriteLine($"Degrees: {totalDegrees:f2}");
+
+            if (totalDegrees < 38)
+            {
+                Console.WriteLine("Not good, you should baking!");
+            }
+            else if (totalDegrees > 38 && totalDegrees < 42)
+            {
+                Console.WriteLine("Super!");
+            }
+            else if (totalDegrees > 42)
+            {
+                Console.WriteLine("Dilution with distilled water!");
+            }
+
+        }
+    }
+}
