332b1520ff9b883d7983998dccc839550db50ded
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-05 16:14:21 +0200

    Programming Basics Exam - 05 November 2017

---
+++ Programming Basics Exam - 05 November 2017/01.WireNet/01.WireNet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.WireNet
+{
+    class Program
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+
+            double wireLength = 2 * length + 2 * width;
+            double wirePrice = wireLength * price;
+            double wireArea = wireLength * height;
+            double wireWeight = wireArea * weight;
+
+            Console.WriteLine($"{wireLength}");
+            Console.WriteLine($"{wirePrice:f2}");
+            Console.WriteLine($"{wireWeight:f3}");
+        }
+    }
+}
