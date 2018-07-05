0a75542a62fb46844737518a7e4af78c8756fa50
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-05 15:55:57 +0200

    Programming Basics Exam - 17 September 2017

---
+++ Programming Basics Exam - 17 September 2017/06.TheSongOfTheWheels/06.TheSongOfTheWheels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06.TheSongOfTheWheels
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            string password = null;
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    for (int c = 1; c <= 9; c++)
+                    {
+                        for (int d = 1; d < 9; d++)
+                        {
+                            if (a < b && c > d )
+                            {
+                                int sum = a * b + c * d;
+                                if (sum == n)
+                                {
+                                    Console.Write($"{a}{b}{c}{d} ");
+                                    count++;
+                                    if (count == 4)
+                                    {
+                                        password = $"{a}{b}{c}{d}";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (count > 0)
+                Console.WriteLine();
+            if (password != null)
+                Console.WriteLine($"Password: {password}");
+            else
+                Console.WriteLine("No!");
+        }
+    }
+}
