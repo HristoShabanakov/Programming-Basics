47aed5798f5a7b5af55d87d4c6710bbcc6e4a5f9
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-10 21:04:17 +0200

    Programming Basics Exam - 16 December 2017

---
+++ Programming Basics Exam - 16 December 2017/01.ChristmasPreparation/01.ChristmasPreparation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.ChristmasPreparation
+{
+    class Program
+    {
+        static void Main()
+        {
+            double rolls = double.Parse(Console.ReadLine());
+            double cotton = double.Parse(Console.ReadLine());
+            double glue = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double rollsPrice = rolls * 5.8;
+            double cottonPrice = cotton * 7.2;
+            double gluePrice = glue * 1.2;
+            double totalPrice = rollsPrice + cottonPrice + gluePrice;
+            double discountPrice = totalPrice - ((totalPrice * discount) / 100);
+
+            Console.WriteLine($"{discountPrice:f3}");
+        }
+    }
+}
