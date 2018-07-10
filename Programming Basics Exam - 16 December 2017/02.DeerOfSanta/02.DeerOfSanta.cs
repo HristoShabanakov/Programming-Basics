d9dd6f58a17358c69e2fd5391c2704975f76cbdb
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-10 22:24:37 +0200

    Programming Basics Exam - 16 December 2017

---
+++ Programming Basics Exam - 16 December 2017/02.DeerOfSanta/02.DeerOfSanta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.DeerOfSanta
+{
+    class Program
+    {
+        static void Main()
+        {
+            double days = double.Parse(Console.ReadLine());
+            double food = double.Parse(Console.ReadLine());
+            double foodPerDayFirstDeer = double.Parse(Console.ReadLine());
+            double foodPerDaySecondDeer = double.Parse(Console.ReadLine());
+            double foodPerDayThirdDeer = double.Parse(Console.ReadLine());
+
+            double firstDeer = days * foodPerDayFirstDeer;
+            double secondDeer = days * foodPerDaySecondDeer;
+            double thirdDeer = days * foodPerDayThirdDeer;
+            double totalFood = firstDeer + secondDeer + thirdDeer;
+
+            if (food > totalFood)
+            {
+                Console.WriteLine($"{Math.Floor(food - totalFood)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(totalFood-food)} more kilos of food are needed.");
+            }
+
+        }
+    }
+}
