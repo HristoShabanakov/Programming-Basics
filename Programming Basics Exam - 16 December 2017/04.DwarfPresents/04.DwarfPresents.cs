70498ede083d8c408bb49ef58e71ebb336e87759
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-14 07:54:03 +0200

    Programming Basics Exam - 16 December 2017

---
+++ Programming Basics Exam - 16 December 2017/04.DwarfPresents/04.DwarfPresents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.DwarfPresents
+{
+    class Program
+    {
+        static void Main()
+        {
+            int dwarfs = int.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            double budget = 0;
+
+            for (int i = 0; i < dwarfs; i++)
+            {
+                string gifts = Console.ReadLine();
+                if (gifts == "sand clock")
+                    budget += 2.2;
+                else if (gifts == "magnet")
+                    budget += 1.5;
+                else if (gifts == "cup")
+                    budget += 5;
+                else if (gifts == "t-shirt")
+                    budget += 10;
+            }
+            if (money >= budget)
+                Console.WriteLine($"Santa Claus has {money - budget:f2} more leva left! ");
+            else
+                Console.WriteLine($"Santa Claus will need {budget - money:f2} more leva. ");
+        }
+    }
+}
