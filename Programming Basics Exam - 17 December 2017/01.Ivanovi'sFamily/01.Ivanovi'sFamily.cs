8f5a58e9fec9d249728f8674e47baf99007d1700
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-14 08:23:29 +0200

    Programming Basics Exam - 17 December 2017

---
+++ Programming Basics Exam - 17 December 2017/01.Ivanovi'sFamily/01.Ivanovi'sFamily.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Ivanovi_sFamily
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double firstKidPrice = double.Parse(Console.ReadLine());
+            double secondKidPrice = double.Parse(Console.ReadLine());
+            double thirdKidPrice = double.Parse(Console.ReadLine());
+
+            double totalKidsPrice = firstKidPrice + secondKidPrice + thirdKidPrice;
+            double total = budget - totalKidsPrice;
+            double donationAmount = total - (total * 0.10);
+            Console.WriteLine(donationAmount);
+
+        }
+    }
+}
