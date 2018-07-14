ff3a1216b353fd16b74ff59d9a690f4080ec532a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-14 09:10:46 +0200

    Programming Basics Exam - 17 December 2017

---
+++ Programming Basics Exam - 17 December 2017/02.NewYearsEveParty/02.NewYearsEveParty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.NewYearsEveParty
+{
+    class Program
+    {
+        static void Main()
+        {
+            double guests = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double covert = guests * 20;
+            double remainingMoney = budget - covert;
+
+            if (budget >= covert)
+            {
+                double donationMoney = remainingMoney - (remainingMoney * 0.40);
+                double fireworksMoney = remainingMoney - donationMoney;
+                Console.WriteLine($"Yes! {Math.Round(fireworksMoney)} lv are for fireworks and {Math.Round(donationMoney)} lv are for donation.");
+            }
+            else if (budget < covert)
+            {
+                Console.WriteLine($"They won't have enough money to pay the covert. They will need {Math.Round(covert - budget)} lv more.");
+            }
+        }
+    }
+}
