7ba1cf49578a5827497e6630eef5f7ab6a7bb4e0
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-24 22:39:08 +0200

    Programming Basics Online Exam - 7 January 2018

---
+++ Programming Basics Online Exam - 7 January 2018/02.FriendlyTrip/02.FriendlyTrip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.FriendlyTrip
+{
+    class Program
+    {
+        static void Main()
+        {
+            double kilometers = double.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+            double fuelPrice = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double fuelconsumption = (kilometers * fuel) / 100.0;
+            double totalCost = fuelconsumption * fuelPrice;
+
+            if (budget >= totalCost)
+                Console.WriteLine($"You can take a trip. {budget - totalCost:f2} money left.");
+            else
+                Console.WriteLine($"Sorry, you cannot take a trip. Each will receive {budget / 5:f2} money.");
+        }
+    }
+}
