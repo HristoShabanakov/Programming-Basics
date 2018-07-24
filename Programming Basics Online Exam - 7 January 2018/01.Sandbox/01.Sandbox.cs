5e0bd8aae07a08c5dc1fa89bb0029aa6f58ad0dc
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-24 22:38:18 +0200

    Programming Basics Online Exam - 7 January 2018

---
+++ Programming Basics Online Exam - 7 January 2018/01.Sandbox/01.Sandbox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.Sandbox
+{
+    class Program
+    {
+        static void Main()
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double sand = double.Parse(Console.ReadLine());
+            double wood = double.Parse(Console.ReadLine());
+
+            double area = height * width;
+            double sandstoneArea = (height - (0.2 + 0.2)) * (width - (0.2 + 0.2));
+            double curbStoneArea = area - sandstoneArea;
+            double sandKg = Math.Ceiling(sandstoneArea * 20);
+            double woodMeters = Math.Ceiling(curbStoneArea / (2.2 * 0.2));
+
+            double sandPrice = sandKg * sand;
+            double woodPrice = woodMeters * wood;
+            double totalPrice = sandPrice + woodPrice;
+
+            Console.WriteLine($"{totalPrice:f2}");
+            
+        }
+    }
+}
