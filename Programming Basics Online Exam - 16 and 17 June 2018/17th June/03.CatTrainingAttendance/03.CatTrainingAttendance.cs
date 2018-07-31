9e8d6429e95498eb3853e57597f7812c1643cb5a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-31 15:57:07 +0200

    Programming Basics Online Exam - 16 and 17 June 2018

--- Programming Basics Online Exam - 16 and 17 June 2018/17th June/03.CatTrainingAttendance/03.CatTrainingAttendance.cs
+++ Programming Basics Online Exam - 16 and 17 June 2018/17th June/03.CatTrainingAttendance/03.CatTrainingAttendance.cs
@@ -6,7 +6,7 @@ namespace _03.CatTrainingAttendance
     {
         static void Main()
         {
-            int startTime = int.Parse(Console.ReadLine());
+            int hoursOfBeginning = int.Parse(Console.ReadLine());
             int hoursCheckIn = int.Parse(Console.ReadLine());
             int minutesCheckIn = int.Parse(Console.ReadLine());
             string day = Console.ReadLine();
@@ -14,32 +14,21 @@ namespace _03.CatTrainingAttendance
             double bonusPoints = 0;
 
             if (day == "Monday" || day == "Wednesday" || day == "Friday")
-            {
                 bonusPoints += 0.6;
-            }
             else if (day == "Tuesday" || day == "Thursday" || day == "Saturday")
-            {
                 bonusPoints += 0.8;
-            }
             else if (day == "Sunday")
-            {
                 bonusPoints += 2;
-            }
+
             //If the training starts at 17:00(included) until 18:00
-            if (hoursCheckIn < startTime && startTime <= (startTime - 1))
-            {
+            if (hoursCheckIn < hoursOfBeginning && hoursCheckIn <= (hoursOfBeginning - 1))
                bonusPoints += 1.5;
-            }
             //Starts at 18:00(included) until 18:30
-            else if (hoursCheckIn == startTime && minutesCheckIn <= 30)
-            {
+            else if (hoursCheckIn == hoursOfBeginning && minutesCheckIn <= 30)
                 bonusPoints += 1;
-            }
             //Starts at 18:30 until 22:30(included)
-            else if (hoursCheckIn == startTime && minutesCheckIn > 30 || hoursCheckIn < (hoursCheckIn + 4))
-            {
+            else if (hoursCheckIn == hoursOfBeginning && minutesCheckIn > 30 || hoursCheckIn < (hoursOfBeginning + 4))
                 bonusPoints += 0.5;
-            }
 
             Console.WriteLine($"{bonusPoints:f2}");
         }
