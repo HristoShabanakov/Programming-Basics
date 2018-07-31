d6fb8acdf5917ed3816daebc8fefa30dd1b36932
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-31 16:06:41 +0200

    Programming Basics Online Exam - 29 July 2018

---
+++ Programming Basics Online Exam - 29 July 2018/05.GameInfo/05.GameInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.GameInfo
+{
+    class Program
+    {
+        static void Main()
+        {
+            string country = Console.ReadLine();
+            double matches = double.Parse(Console.ReadLine());
+            double totalTime = 0;
+            double extraTime = 0;
+            double penalties = 0;
+
+            for (int i = 0; i < matches; i++)
+            {
+                double minutes = double.Parse(Console.ReadLine());
+                totalTime += minutes;
+                if (minutes > 90 && minutes <= 120)
+                {
+                    extraTime++;
+                }
+                else if (minutes > 120)
+                {
+                    penalties++;
+                }
+            }
+
+            Console.WriteLine($"{country} has played {totalTime} minutes. Average minutes per game: {totalTime / matches:f2}");
+            Console.WriteLine($"Games with penalties: {penalties}");
+            Console.WriteLine($"Games with additional time: {extraTime}");
+        }
+    }
+}
