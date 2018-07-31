7624f8fee3c6be652be7cd919fafe4b6ec931857
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-31 16:04:23 +0200

    Programming Basics Online Exam - 29 July 2018

---
+++ Programming Basics Online Exam - 29 July 2018/01.StadiumIncome/01.StadiumIncome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.StadiumIncome
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sectors = double.Parse(Console.ReadLine());
+            double capacity = double.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+
+            double incomePerSector = (capacity * ticketPrice) / sectors;
+            double totalIncome = incomePerSector * sectors;
+            double moneyForCharity = (totalIncome - (incomePerSector * 0.75)) / 8;
+
+            Console.WriteLine($"Total income - {totalIncome:f2} BGN");
+            Console.WriteLine($"Money for charity - {moneyForCharity:f2} BGN");
+        }
+    }
+}
