e771637144d605ec8fa37ae89e7491c30ddb6925
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-07-31 15:58:03 +0200

    Programming Basics Online Exam - 16 and 17 June 2018

--- Programming Basics Online Exam - 16 and 17 June 2018/17th June/04.CatFood/04.CatFood.cs
+++ Programming Basics Online Exam - 16 and 17 June 2018/17th June/04.CatFood/04.CatFood.cs
@@ -13,32 +13,30 @@ namespace _04.CatFood
             int thirdGroup = 0;
             double totalCatFood = 0;
 
-            double foodPrice = 12.45;
-
-            for (int i = 1; i <=n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 double input = double.Parse(Console.ReadLine());
+                totalCatFood += input;
 
                 if (input >= 100 && input < 200)
                 {
                     firstGroup++;
-                    totalCatFood += input;
                 }
-                else if(input >= 200 && input < 300)
+                else if (input >= 200 && input < 300)
                 {
                     secondGroup++;
-                    totalCatFood += input;
                 }
                 else if (input >= 300 && input < 400)
                 {
                     thirdGroup++;
-                    totalCatFood += input;
                 }
             }
+
             Console.WriteLine($"Group 1: {firstGroup} cats.");
             Console.WriteLine($"Group 2: {secondGroup} cats.");
             Console.WriteLine($"Group 3: {thirdGroup} cats.");
-            Console.WriteLine($"Price for food per day: {totalCatFood / 1000 * foodPrice:f2} lv.");
+            totalCatFood = Math.Round(((totalCatFood / 1000) * 12.45),2);
+            Console.WriteLine($"Price for food per day: {totalCatFood} lv.");
         }
     }
 }
