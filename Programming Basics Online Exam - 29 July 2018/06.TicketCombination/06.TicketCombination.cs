74e67c7d5235839cdecc7e5ef2d79f75a6043459
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-09-18 00:05:49 +0200

    Programming Basics Online Exam - 29 July 2018

---
+++ Programming Basics Online Exam - 29 July 2018/06.TicketCombination/06.TicketCombination.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.TicketCombination
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (char i = 'B'; i <= 'L'; i+=(char)2)
+            {
+                for (char j = 'f'; j >= 'a'; j--)
+                {
+                    for (char k = 'A'; k <= 'C'; k++)
+                    {
+                        for (int l = 1; l <= 10; l++)
+                        {
+                            for (int m = 10; m >= 1; m--)
+                            {
+                                counter++;
+                                if(counter >=input)
+                                {
+                                    Console.WriteLine($"Ticket combination: {i}{j}{k}{l}{m}");
+                                    int totalSum = i + j + k + l + m;
+                                    Console.WriteLine($"Prize: {totalSum} lv.");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
